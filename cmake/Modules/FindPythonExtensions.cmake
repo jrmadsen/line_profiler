7f36e764d0400bb58fa9135aec1d3d0703ea6d1b
Jonathan R. Madsen <jonathanrmadsen@gmail.com>
2021-01-04 23:27:48 -0800

    Update FindPythonExtensions.cmake

--- cmake/Modules/FindPythonExtensions.cmake
+++ cmake/Modules/FindPythonExtensions.cmake
@@ -288,7 +288,7 @@ sys.stdout.write(\";\".join((
     sys.prefix,
     result,
     rel_result,
-    sysconfig.get_config_var('SO')
+    sysconfig.get_config_var('EXT_SUFFIX')
 )))
 ")
 
@@ -327,16 +327,16 @@ function(_set_python_extension_symbol_visibility _target)
   else()
     set(_modinit_prefix "init")
   endif()
-  message("_modinit_prefix:${_modinit_prefix}")
+  # message("_modinit_prefix:${_modinit_prefix}")
   if("${CMAKE_C_COMPILER_ID}" STREQUAL "MSVC")
-    set_target_properties(${_target} PROPERTIES LINK_FLAGS 
+    set_target_properties(${_target} PROPERTIES LINK_FLAGS
         "/EXPORT:${_modinit_prefix}${_target}"
     )
   elseif("${CMAKE_C_COMPILER_ID}" STREQUAL "GNU" AND NOT ${CMAKE_SYSTEM_NAME} MATCHES "Darwin")
     set(_script_path
       ${CMAKE_CURRENT_BINARY_DIR}/CMakeFiles/${_target}-version-script.map
     )
-    file(WRITE ${_script_path} 
+    file(WRITE ${_script_path}
                "{global: ${_modinit_prefix}${_target}; local: *; };"
     )
     set_property(TARGET ${_target} APPEND_STRING PROPERTY LINK_FLAGS
@@ -421,7 +421,7 @@ function(python_extension_module _target)
     endif()
 
     target_link_libraries_with_dynamic_lookup(${_target} ${PYTHON_LIBRARIES})
-    
+
     if(_is_module_lib)
       _set_python_extension_symbol_visibility(${_target})
     endif()
@@ -571,4 +571,3 @@ function(python_modules_header _name)
   endif()
   set(${_include_dirs_var} ${CMAKE_CURRENT_BINARY_DIR} PARENT_SCOPE)
 endfunction()
-
